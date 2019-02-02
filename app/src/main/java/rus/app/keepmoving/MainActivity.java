df80f11cc5a57c2d8e12abc50fccc0d51cd2a2c6
Rustem Serikov <NatrigDev@gmail.com>
2019-02-02 17:03:19 +0300

    LogIn activity style

--- app/src/main/java/rus/app/keepmoving/MainActivity.java
+++ app/src/main/java/rus/app/keepmoving/MainActivity.java
@@ -1,13 +1,34 @@
 package rus.app.keepmoving;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "EXTRA_MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // Метод авторизации
+    public void login(View view) {
+        // действия, совершаемые после нажатия на кнопку
+        // Создаем объект Intent для вызова новой Activity
+        Intent intent = new Intent(this, MenuActivity.class);
+        // Получаем текстовое поле в текущей Activity
+        EditText editText = (EditText) findViewById(R.id.emailInput);
+        // Получае текст данного текстового поля
+        String message = editText.getText().toString();
+        // Добавляем с помощью свойства putExtra объект - первый параметр - ключ,
+        // второй параметр - значение этого объекта
+        intent.putExtra(EXTRA_MESSAGE, message);
+        // запуск activity
+        startActivity(intent);
     }
 }
